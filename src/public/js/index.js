b8f83ea41200a4251d5addeffda96c87d541ab88
Yelban <yelban@gmail.com>
2021-11-25 09:25:45 +0800

    Parcel Web App Boilerplate with The New CSS Reset

---
+++ src/public/js/index.js
@@ -0,0 +1,9 @@
+console.log('index.js');
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('ready');
+});
+
+window.addEventListener("load", function(event) {
+    console.log("load");
+});
